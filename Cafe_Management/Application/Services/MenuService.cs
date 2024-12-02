b65b1d3876271b9431c84a2311923323206412f6
TranTien050820 <trantien5.8.2000@gmail.com>
2024-12-02 22:42:22 +0700

    up

---
+++ Cafe_Management/Application/Services/MenuService.cs
@@ -0,0 +1,32 @@
+﻿using Cafe_Management.Core.Entities;
+using Cafe_Management.Core.Interfaces;
+
+namespace Cafe_Management.Application.Services
+{
+    public class MenuService
+    {
+        private readonly IMenuRepository _menuRepository;
+
+        public MenuService(IMenuRepository menuRepository)
+        {
+            _menuRepository = menuRepository;
+        }
+
+        public async Task<IEnumerable<Menu>> GetMenus(Nullable<int> Menu_ID, Nullable<bool> IsActive)
+        {
+            return await _menuRepository.GetMenus(Menu_ID, IsActive);
+        }
+
+        public async Task AddMenu(Menu menu)
+        {
+            await _menuRepository.AddMenu(menu);
+        }
+
+
+
+        public async Task UpdateMenu(Menu menu)
+        {
+            await _menuRepository.UpdateMenu(menu);
+        }
+    }
+}
